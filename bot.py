51c9fcaf166ef99758f1283af8dcd239898ff28c
khanhtrinh2003 <47718349+khanhtrinh2003@users.noreply.github.com>
2023-01-24 17:53:50 +0700

    update bot

--- bot.py
+++ bot.py
@@ -5,15 +5,13 @@ from typing import List
 import matplotlib.pyplot as plt
 
 
-#import module
-
 from Schedule.tkb22_23 import room, hoc_ke
-from Notification.noti import news
+from Notification.Noti import news
 from Game.game import Game_theo, DoanSo
 from Chatbot.chatbot import AI_BOT, Code, Image, Chat
 
 TOKEN = '' # Put toke in here
-ServerID = "919927035558764574"# Put serverid
+ServerID = "919927035558764574" # Put serverid
 
 class abot(discord.Client):
     def __init__(self):
@@ -21,7 +19,7 @@ class abot(discord.Client):
         self.synced = False
 
     async def on_ready(self):
-        await tree.sync()
+        await tree.sync(guild=discord.Object(id=ServerID))
         self.synced = True
         print('Bot has connected to Discord!')
 
@@ -41,12 +39,13 @@ async def on_message(message):
 @tree.command(
     name='confess', 
     description='Nhắn tin ẩn danh',
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.describe(message='Nhập tin nhắn', file='Gửi hình hoặc file')
 
 async def confess(interaction: discord.Interaction, message: str, file: typing.Optional[discord.Attachment]):    
-    if interaction.guild.id in ServerID:
+    
         await interaction.response.send_message('......', ephemeral=True, delete_after=0.000000000000000000000000000000001)
         await interaction.channel.send(f'{message}')
         await interaction.channel.send(file)
@@ -56,7 +55,7 @@ async def confess(interaction: discord.Interaction, message: str, file: typing.O
 @tree.command(
     name='tim_phong', 
     description='Tìm phòng ở NEU',
-    
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.describe(
@@ -104,7 +103,7 @@ async def timphong(
     building: app_commands.Choice[str], 
     floors: app_commands.Choice[str]
     ):
-    if interaction.guild.id in ServerID:
+    
         res = room(days.value, period.value, building.value, floors.value)
         embedVar = discord.Embed(title="Các phòng có người học", description=res, color=0x15E3E0)
         await interaction.response.send_message(embed=embedVar, ephemeral=True)    
@@ -113,7 +112,7 @@ async def timphong(
 @tree.command(
     name='hoc_ke', 
     description='Học ké ở NEU',
-    
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.describe()
@@ -134,7 +133,7 @@ async def ke(
 @tree.command(
     name='thongbao', 
     description='Thông báo', 
-    
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.choices(
@@ -155,7 +154,7 @@ async def news(interaction: discord.Interaction, thong_tin : app_commands.Choice
 @tree.command(
     name='guess', 
     description='Đoán số nằm trong khoản 0->100', 
-    
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.describe(num="Nhập số")
@@ -168,7 +167,7 @@ async def self(interaction: discord.Interaction, num: int):
 @tree.command(
     name='chien_luoc', 
     description='Chiến lược', 
-    
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.describe(p="Chọn chiến lược")
@@ -185,7 +184,7 @@ async def self(interaction: discord.Interaction, p: float):
 @tree.command(
     name='code', 
     description='Giải đáp về code', 
-    
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.describe(tin_nhan="Nhập tin nhắn")
@@ -194,11 +193,11 @@ async def self(interaction: discord.Interaction, tin_nhan: str):
     await interaction.response.send_message(tin_nhan, ephemeral=True) 
     await interaction.channel.send(Code(tin_nhan))
 
-# Feature 7
+# Feature 8
 @tree.command(
     name='image', 
     description='Hình ảnh', 
-    
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.describe(tin_nhan="Nhập tin nhắn")
@@ -207,11 +206,11 @@ async def self(interaction: discord.Interaction, tin_nhan: str):
     await interaction.response.send_message(tin_nhan, ephemeral=True) 
     await interaction.channel.send(Image(tin_nhan))
 
-# Feature 8
+# Feature 9
 @tree.command(
     name='chat', 
     description='Chat với Bot', 
-    
+    guild=discord.Object(id=ServerID)
     )
 
 @app_commands.describe(tin_nhan="Nhập tin nhắn")
