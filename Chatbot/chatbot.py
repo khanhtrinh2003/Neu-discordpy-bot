eed88e82eb426a7733ec16a0a117cf1707c71130
khanhtrinh2003 <47718349+khanhtrinh2003@users.noreply.github.com>
2023-01-24 15:56:37 +0700

    Update chatbot

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,38 +1,47 @@
 import openai
 import requests
-import re
-openai.api_key = "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"
+
+openai.api_key = ""
 
 def generate_response(prompt, temperature):
     completions = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=4000, # 4000 is the maximum number of tokens for the davinci 003 GPT3 model
+        max_tokens=4000,
         n=1,
         stop=None,
         temperature=temperature,
     )
 
-    if 'error' in completions:
-        return completions['error']['message']
-    else:
-        message = completions.choices[0].text
+    message = completions.choices[0].text
     return message
-
 
 def Chat(tin_nhan):
     prompt = tin_nhan
-    response_lines = generate_response(prompt, 0.5)
-    if isinstance(response_lines, str):
-        return (response_lines)
-    else:
-        for line in response_lines:
-            return (line)    
+    message = generate_response(prompt, 0.5)
+    return message
 
+def AI_BOT(tin_nhan):        
+    if tin_nhan.startswith("#"):
+        pass 
+    else:
+        prompt = tin_nhan
+        message = generate_response(prompt, 0.5)
+        return message
+    
 def Code(tin_nhan):
     prompt = tin_nhan
-    response_lines = generate_response(prompt, 0.5)
-    return f"```{response_lines}```"
+    completions = openai.Completion.create(
+        engine="code-davinci-002",
+        prompt=prompt,
+        max_tokens=4000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return f"```{message}```"
+
 
 def Image(tin_nhan):
     prompt = tin_nhan
@@ -46,16 +55,3 @@ def Image(tin_nhan):
     else:
         image_url = response['data'][0]['url']
         return image_url
-
-def AI_BOT(tin_nhan):        
-    if tin_nhan.startswith("#"):
-        pass 
-    else:
-        prompt = tin_nhan
-        response_lines = generate_response(prompt, 0.5)
-        if isinstance(response_lines, str):
-            return (response_lines)
-        else:
-            for line in response_lines:
-                return (line)
-        
