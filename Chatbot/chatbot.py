7b179384d71c6120c904e65696ad46e5e74bc8bf
khanhtrinh2003 <nguyenduckhanhtrinh@gmail.com>
2023-01-25 08:51:13 +0700

    update chat bot

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -3,37 +3,33 @@ import requests
 
 openai.api_key = ""
 
-def generate_response(prompt, temperature):
+def generate_response(tin_nhan, temperature):
     completions = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=prompt,
+        prompt=tin_nhan,
         max_tokens=4000,
         n=1,
         stop=None,
         temperature=temperature,
     )
-
     message = completions.choices[0].text
     return message
 
-def Chat(tin_nhan):
-    prompt = tin_nhan
-    message = generate_response(prompt, 0.5)
+def Chat(tin_nhan): 
+    message = generate_response(tin_nhan, 0.5)
     return message
 
 def AI_BOT(tin_nhan):        
     if tin_nhan.startswith("#"):
         pass 
     else:
-        prompt = tin_nhan
-        message = generate_response(prompt, 0.5)
+        message = generate_response(tin_nhan, 0.5)
         return message
     
 def Code(tin_nhan):
-    prompt = tin_nhan
     completions = openai.Completion.create(
         engine="code-davinci-002",
-        prompt=prompt,
+        prompt=tin_nhan,
         max_tokens=4000,
         n=1,
         stop=None,
@@ -44,10 +40,9 @@ def Code(tin_nhan):
 
 
 def Image(tin_nhan):
-    prompt = tin_nhan
     api_url = "https://api.openai.com/v1/images/generations"
     api_key = {"Authorization": f"Bearer {openai.api_key}"}
-    data = {"model": "image-alpha-001", "prompt": prompt, "num_images": 1, "size": "1024x1024", "response_format": "url"}
+    data = {"model": "image-alpha-001", "prompt": tin_nhan, "num_images": 1, "size": "1024x1024", "response_format": "url"}
     response = requests.post(api_url, headers=api_key, json=data).json()
     if 'error' in response:
         error_message = response['error']['message']
